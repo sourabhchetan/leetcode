9bfe4493eee5bc391d797bac4f558c23ce50c682
sourabhchetan <sourabhchetan1605hotmail.com>
2026-06-11 18:25:30 +0530

    Solve 020

---
+++ Easy/020_valid parenthense.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack.pop() != pairs[ch]:
+                    return False
+            else:
+                stack.append(ch)
+
+        return len(stack) == 0
