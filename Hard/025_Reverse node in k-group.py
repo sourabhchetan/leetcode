89d0643fee534b08a59fc71e2e788891b8519994
sourabhchetan <sourabhchetan1605hotmail.com>
2026-06-16 14:50:21 +0530

    Solve 25

---
+++ Hard/025_Reverse node in k-group.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        group_prev = dummy
+
+        while True:
+            kth = group_prev
+
+            # Find the kth node
+            for _ in range(k):
+                kth = kth.next
+                if not kth:
+                    return dummy.next
+
+            group_next = kth.next
+
+            # Reverse the group
+            prev = group_next
+            curr = group_prev.next
+
+            while curr != group_next:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+
+            temp = group_prev.next
+            group_prev.next = kth
+            group_prev = temp
