46fa7f052fcfc0d08032006584c4924d74cf7ed8
sourabhchetan <sourabhchetan1605hotmail.com>
2026-07-02 12:12:29 +0530

    Solve 41

---
+++ Hard/41_first missing positive.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+
+        i = 0
+        while i < n:
+            correct = nums[i] - 1
+
+            if 1 <= nums[i] <= n and nums[i] != nums[correct]:
+                nums[i], nums[correct] = nums[correct], nums[i]
+            else:
+                i += 1
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        return n + 1
