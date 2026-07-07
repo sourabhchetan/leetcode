2ca6b38e255cec70ed75761f576f6af36100664e
sourabhchetan <sourabhchetan1605hotmail.com>
2026-07-07 17:41:54 +0530

    Solve 046

---
+++ Medium/046_permutation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path, used)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([], [False] * len(nums))
+        return result
