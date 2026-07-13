ffd72f9d6c25759822a6d96dc69e0de52d79106c
sourabhchetan <sourabhchetan1605hotmail.com>
2026-07-13 14:03:46 +0530

    Solve 52

---
+++ Hard/52_N-queen2.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        cols = set()
+        diag1 = set()  # row - col
+        diag2 = set()  # row + col
+
+        self.count = 0
+
+        def backtrack(row):
+            if row == n:
+                self.count += 1
+                return
+
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1)
+
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0)
+        return self.count
