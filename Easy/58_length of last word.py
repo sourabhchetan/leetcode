829ddd23ee2d0fedf7a32baa8edc8dc0c2bb8bc4
sourabhchetan <sourabhchetan1605hotmail.com>
2026-07-19 13:25:11 +0530

    Solve 58

---
+++ Easy/58_length of last word.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = len(s) - 1
+
+        # Skip trailing spaces
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        length = 0
+
+        # Count characters of the last word
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+
+        return length
