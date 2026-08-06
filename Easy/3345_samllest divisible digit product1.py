7a3b749bb768b3a4aeb5e9b8c4ffb8255727a78e
sourabhchetan <sourabhchetan1605hotmail.com>
2026-08-06 15:43:08 +0530

    Solve 3345

---
+++ Easy/3345_samllest divisible digit product1.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        :type n: int
+        :type t: int
+        :rtype: int
+        """
+        while True:
+            product = 1
+            x = n
+
+            while x > 0:
+                product *= x % 10
+                x //= 10
+
+            if product % t == 0:
+                return n
+
+            n += 1
