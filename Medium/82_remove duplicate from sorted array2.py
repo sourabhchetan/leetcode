50d457cd3b353b5f306d318ba79820684c96107a
sourabhchetan <sourabhchetan1605hotmail.com>
2026-08-14 12:27:20 +0530

    Solve 82

---
+++ Medium/82_remove duplicate from sorted array2.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+        curr = head
+
+        while curr:
+            # Check if current value has duplicates
+            if curr.next and curr.val == curr.next.val:
+                duplicate = curr.val
+
+                # Skip all nodes with this value
+                while curr and curr.val == duplicate:
+                    curr = curr.next
+
+                prev.next = curr
+            else:
+                prev = curr
+                curr = curr.next
+
+        return dummy.next
