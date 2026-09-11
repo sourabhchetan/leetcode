23cd02b1f47fa925b8ec3f7d1fbf8ef4a6b08fb8
Sourabh chetan <sourabhchetan1605@hotmail.com>
2026-09-11 12:20:08 +0530

    Implement solution to count unique 3-digit even numbers

---
+++ Easy/3483_unique 3-digit even number.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def totalNumbers(self, digits):
+        numbers = set()
+        n = len(digits)
+
+        for i in range(n):
+            # First digit cannot be 0
+            if digits[i] == 0:
+                continue
+
+            for j in range(n):
+                if j == i:
+                    continue
+
+                for k in range(n):
+                    if k == i or k == j:
+                        continue
+
+                    # Last digit must be even
+                    if digits[k] % 2 != 0:
+                        continue
+
+                    num = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    numbers.add(num)
+
+        return len(numbers)
