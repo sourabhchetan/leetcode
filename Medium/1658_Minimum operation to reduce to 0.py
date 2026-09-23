3524591bb00be97271a9e6b0a882e2848c5e58d7
Sourabh chetan <sourabhchetan1605@hotmail.com>
2026-09-23 11:43:29 +0530

    Add solution for minimum operations to reduce to zero
    Implement a solution to find the minimum operations to reduce an array to zero by removing elements from either end.

---
+++ Medium/1658_Minimum operation to reduce to 0.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def minOperations(self, nums, x):
+        total = sum(nums)
+        target = total - x
+
+        # We need to remove everything
+        if target == 0:
+            return len(nums)
+
+        # Impossible target
+        if target < 0:
+            return -1
+
+        left = 0
+        curr_sum = 0
+        max_len = -1
+
+        for right in range(len(nums)):
+            curr_sum += nums[right]
+
+            while curr_sum > target and left <= right:
+                curr_sum -= nums[left]
+                left += 1
+
+            if curr_sum == target:
+                max_len = max(max_len, right - left + 1)
+
+        if max_len == -1:
+            return -1
+
+        return len(nums) - max_len
