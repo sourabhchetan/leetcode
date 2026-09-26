636ff5cfe56e191cfe49ffc372abf05c1180d4a2
Sourabh chetan <sourabhchetan1605@hotmail.com>
2026-09-26 21:30:24 +0530

    Implement bracket evaluation in a string

---
+++ Medium/1807_evaluate the bracket pairs of  a string.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def evaluate(self, s, knowledge):
+        # Convert knowledge into a dictionary
+        mp = {}
+        for key, value in knowledge:
+            mp[key] = value
+
+        ans = []
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = i + 1
+
+                while s[j] != ')':
+                    j += 1
+
+                key = s[i + 1:j]
+
+                if key in mp:
+                    ans.append(mp[key])
+                else:
+                    ans.append('?')
+
+                i = j + 1
+            else:
+                ans.append(s[i])
+                i += 1
+
+        return ''.join(ans)
